9df13f8cc91332f794ed92da541f57526a464356
Siddhartha Borah <siddhartha.borah@outlook.com>
2025-05-06 22:17:24 +0530

    enable vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'; // your global styles
+import { Analytics } from "@vercel/analytics/react"
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
